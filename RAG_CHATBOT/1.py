88c25bc11805ac4d456f7d0efb0a57f8f3f88544
Akshar Patel <109402115+Akshar106@users.noreply.github.com>
2025-06-26 23:35:03 +0530

    Delete RAG_CHATBOT/1.py

--- RAG_CHATBOT/1.py
+++
@@ -1,8 +0,0 @@
-import os
-
-LAW_PDF_FOLDER = "./data/Law/"
-
-if os.path.exists(LAW_PDF_FOLDER):
-    print(f"✅ Folder exists: {LAW_PDF_FOLDER}")
-else:
-    print(f"❌ Folder does NOT exist: {LAW_PDF_FOLDER}")
